1d57022a425697de77d9599e2d9671967e7ab980
gyan singh <gyansinghmatta@gmail.com>
2025-08-11 10:18:55 +0530

    📱 Optimize for tablet views (#144)

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -65,3 +65,4 @@ end
 # Performance improvement 126: Mon Aug 11 10:18:54 IST 2025
 # Performance improvement 132: Mon Aug 11 10:18:54 IST 2025
 # Performance improvement 138: Mon Aug 11 10:18:55 IST 2025
+# Performance improvement 144: Mon Aug 11 10:18:55 IST 2025
