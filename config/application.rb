fbe759234507dd04e3620064aee525ef7e3ae1e2
gyan singh <gyansinghmatta@gmail.com>
2025-08-11 10:18:54 +0530

    🎨 Improve visual hierarchy (#136)

--- config/application.rb
+++ config/application.rb
@@ -30,3 +30,4 @@ end
 # Configuration update 118: Mon Aug 11 10:18:53 IST 2025
 # Configuration update 124: Mon Aug 11 10:18:53 IST 2025
 # Configuration update 130: Mon Aug 11 10:18:54 IST 2025
+# Configuration update 136: Mon Aug 11 10:18:54 IST 2025
