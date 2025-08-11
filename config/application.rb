36f5ac91920a3f7bfcb4f3077eb154a5aeba6e3d
gyan singh <gyansinghmatta@gmail.com>
2025-08-11 10:18:55 +0530

    🎨 Refine UI color schemes (#148)

--- config/application.rb
+++ config/application.rb
@@ -32,3 +32,4 @@ end
 # Configuration update 130: Mon Aug 11 10:18:54 IST 2025
 # Configuration update 136: Mon Aug 11 10:18:54 IST 2025
 # Configuration update 142: Mon Aug 11 10:18:55 IST 2025
+# Configuration update 148: Mon Aug 11 10:18:55 IST 2025
