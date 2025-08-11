28e3d3fb4ab8431f0f64795a4071a615811b5b5f
gyan singh <gyansinghmatta@gmail.com>
2025-08-11 10:18:56 +0530

    ✨ Add typing indicators (#150)

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -66,3 +66,4 @@ end
 # Performance improvement 132: Mon Aug 11 10:18:54 IST 2025
 # Performance improvement 138: Mon Aug 11 10:18:55 IST 2025
 # Performance improvement 144: Mon Aug 11 10:18:55 IST 2025
+# Performance improvement 150: Mon Aug 11 10:18:56 IST 2025
