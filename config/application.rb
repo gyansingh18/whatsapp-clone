3837f60bc4289d708596519ee972af7526ba79ba
gyan singh <gyansinghmatta@gmail.com>
2025-08-11 10:18:54 +0530

    📱 Optimize for tablet views (#130)

--- config/application.rb
+++ config/application.rb
@@ -29,3 +29,4 @@ end
 # Configuration update 112: Mon Aug 11 10:18:52 IST 2025
 # Configuration update 118: Mon Aug 11 10:18:53 IST 2025
 # Configuration update 124: Mon Aug 11 10:18:53 IST 2025
+# Configuration update 130: Mon Aug 11 10:18:54 IST 2025
